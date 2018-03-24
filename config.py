532f976c5e0b5f2a21a9d7ba1f79ae9d19f97b1c
mofarah3 <34014870+mofarah3@users.noreply.github.com>
2018-03-24 09:03:27 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+apikey = "9e861b2a772ee55acbe86461ecea57"
+#gkey = "AIzaSyDnwXHq0WloDkU_3rruJWeuJZfgmXBvkhg"
+
+# Dependencies
+#import json
+#import tweepy 
+
+# Twitter API Keys
+api_keyTwitter = "H9KK5JlIitULfiK8ptW28XIRc"
+api_secretTwitter = "cNZBXwLQzj6oJjtqC7cq4TpkSt4QtTqkpkU5gEoPf84bDScqsy"
+access_tokenTwitter = "702584846-ZLWVax4Qc63KHiOEBU4Sn0CEjEixbpIbHzTJTdIa"
+access_tokensecretTwitter = "KujAewoldKATCg4S26JJNZ9xQpXmzMx1TVk44i2v4jWgi"
